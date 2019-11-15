2e07fec877ea313442f4f654bf3417b35d643dc1
Ran Wei <Ran.Wei@data61.csiro.au>
2019-11-15 17:38:01 +1100

    some modification

--- read_9250_output.m
+++ read_9250_output.m
@@ -29,7 +29,31 @@ function data = read_9250_output(file)
         %   note: for this option and the below, magnet values are already
         %   being offsetted by the bias values.
     elseif c == 19 % current output from kris code
-        data.delta_t = ones(1, n)*50;
+        % process to remove NAN values. probably needs to go through all 19
+        % elements
+%         for k = 1:c
+%             id = find(isnan(d(:, k)));
+%             if ~isempty(id)
+%                 d(id, :) = [];
+%             end
+%         end
+        n = size(d, 1);
+        id = find(isnan(d));
+        if sum(id) ~= 0
+            ids = mod(id, n);
+            id0 = ids == 0;
+            idnot0 = ids(~id0);
+            if ~isempty(idnot0)
+                id = unique(idnot0);
+                d(id, :) = [];
+            end
+            %   there is a chance that id is at the last line of the 
+            if ~isempty(id0)
+                %   only one lastline exists, so we can do it once
+                d(end, :) = [];
+            end
+        end
+        
         data.temp = d(:, 1);
         data.delta_t = d(:, 2);
         data.acc = d(:, 3:5);
@@ -51,6 +75,7 @@ function data = read_9250_output(file)
         %   being offsetted by the bias values.
         % data.bias = [242.33 301.695 -426.095];
     end
+    
     
     
 %     %   if the magnetometer data is not calibrated, show its distribution
